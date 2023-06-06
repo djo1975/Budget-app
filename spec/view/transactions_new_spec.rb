c138c1abec16a7afc781445ff3827df7c5220304
djo1975 <mladanilic1975@gmail.com>
2023-06-06 12:23:48 +0200

    Tests tests tests

---
+++ spec/view/transactions_new_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'transactions/new', type: :view do
+  let(:user) { User.create(name: 'John Doe', email: 'john@example.com', password: 'password') }
+  let(:category) { Category.create(name: 'Category 1', user: user) }
+  let(:transaction) { Transaction.new }
+  
+  before do
+    assign(:category, category)
+    assign(:transaction, transaction) # Dodeli promenljivu `@transaction`
+    assign(:total_amount, 30)
+    allow(view).to receive(:current_user).and_return(user)
+    render
+  end
+
+  it 'displays the heading' do
+    expect(rendered).to have_selector('h1', text: 'New Transaction')
+  end
+
+  it 'renders the transaction form' do
+    expect(rendered).to have_selector('form') do |form|
+      expect(form).to have_field('Name')
+      expect(form).to have_field('Amount')
+      expect(form).to have_button('Create Transaction')
+    end
+  end
+
+  it 'renders a link to go back' do
+    expect(rendered).to have_link('Back', href: category_transactions_path(category))
+  end
+end
---
+++ spec/view/transactions_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe 'transactions/index', type: :view do
+  let(:user) { User.create(name: 'John Doe', email: 'john@example.com', password: 'password') }
+  let(:category) { Category.create(name: 'Category 1', user: user) }
+  let!(:transaction1) { Transaction.create(name: 'Transaction 1', amount: 10, author: user) }
+  let!(:transaction2) { Transaction.create(name: 'Transaction 2', amount: 20, author: user) }
+
+  before do
+    assign(:category, category)
+    assign(:transactions, [transaction1, transaction2])
+    assign(:total_amount, 30)
+    allow(view).to receive(:current_user).and_return(user)
+    render
+  end
+
+  it 'displays the heading' do
+    expect(rendered).to have_selector('h1', text: 'Transactions')
+  end
+
+  it 'displays the total amount' do
+    expect(rendered).to have_selector('p', text: 'Total Amount: 30')
+  end
+
+  it 'displays the list of transactions' do
+    expect(rendered).to have_selector('ul') do |ul|
+      expect(ul).to have_selector('li', text: 'Transaction 1 - 10')
+      expect(ul).to have_selector('li', text: 'Transaction 2 - 20')
+    end
+  end
+
+  it 'renders a link to add a new transaction' do
+    expect(rendered).to have_link('Add a new transaction', href: new_category_transaction_path(category))
+  end
+
+  it 'renders a link to go back' do
+    expect(rendered).to have_link('Back', href: authenticated_root_path)
+  end
+end
