111070ce37d1a7a064a2d0ffd2e178e50b330bb6
djo1975 <mladanilic1975@gmail.com>
2023-06-08 14:49:07 +0200

    Deploing is a very hard job

--- db/migrate/20230603140602_rename_transaction_id_in_transaction_categories.rb
+++
@@ -1,5 +0,0 @@
-class RenameTransactionIdInTransactionCategories < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :transaction_categories, :transaction_id, :transaction_record_id
-  end
-end
