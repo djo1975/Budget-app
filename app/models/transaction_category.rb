ab2718ac19a8eea4e5a79615093e1c4833bd4e11
djo1975 <mladanilic1975@gmail.com>
2023-06-08 15:03:38 +0200

    Deploing is a very hard job

--- app/models/transaction_category.rb
+++ app/models/transaction_category.rb
@@ -1,6 +1,6 @@
 class TransactionCategory < ApplicationRecord
-  belongs_to :transaction_record, class_name: 'Transaction', foreign_key: 'transaction_id'
+  belongs_to :transaction, class_name: 'Transaction', foreign_key: 'transaction_id'
   belongs_to :category
 
-  has_many :transactions, through: :transaction_record
+  has_many :transactions, through: :transaction
 end
